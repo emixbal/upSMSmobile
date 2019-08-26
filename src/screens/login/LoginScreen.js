d3bbd0e2b13fd315a5fe0524a6e08a590721a52d
iqbal <emixbal@gmail.com>
2019-08-26 16:43:46 +0700

    create login screen

---
+++ src/screens/login/LoginScreen.js
@@ -0,0 +1,42 @@
+import React, {Component, Fragment} from 'react';
+import {StyleSheet} from 'react-native';
+import { Container, Content, Body, Title, Form, Input, Item, Header, Button, Text} from 'native-base';
+
+
+class LoginScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Login',
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            email: '',
+            password: ''
+        }
+    }
+
+    handleButtonLogin(){
+        this.props.navigation.navigate('Home')
+    }
+
+    render(){
+        return(
+            <Container>
+                <Content>
+                    <Form>
+                        <Item>
+                            <Input placeholder="email" />
+                        </Item>
+                        <Item>
+                            <Input placeholder="password" />
+                        </Item>
+                        <Button info onPress={()=>this.handleButtonLogin()}><Text> Login </Text></Button>
+                    </Form>
+                </Content>
+            </Container>
+        )
+    }
+}
+
+export default LoginScreen;
