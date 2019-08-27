6d80de76dea3297b4ac5acdbcbcf2b3d12df070e
iqbal <emixbal@gmail.com>
2019-08-27 17:12:58 +0700

    add asyncstorage logic

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,31 +1,80 @@
 import React, {Component, Fragment} from 'react';
-import {StyleSheet} from 'react-native';
-import { Container, Content, Button, Text} from 'native-base';
+import {AsyncStorage} from 'react-native';
+import { Container, Content, Button, Text, View} from 'native-base';
 
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
-        // title: 'UpSMS Dashboard',
-        header: null,
+        title: 'UpSMS Dashboard',
     };
     constructor(props) {
         super(props);
         this.state = {
-            loading: false,
+            isLoading: true,
             email: '',
-            password: ''
+            password: '',
+            token:''
+        }
+    }
+
+    async componentDidMount(){
+        try {
+            const token = await AsyncStorage.getItem('token');
+            if(token == null){
+                this.props.navigation.navigate('Login')
+                await this.setState({
+                    isLoading:false,
+                    token: ''
+                })
+            } else{
+                await this.setState({
+                    isLoading:false,
+                    token: token
+                })
+            }
+        } catch (e) {
+            alert('terjadi kesalahan');       
+        }
+    }
+
+    async handleLogoutButton(){
+        try {
+            await AsyncStorage.removeItem('token')
+            await this.props.navigation.navigate('Login')
+            await this.setState({
+                token: ''
+            })
+        } catch (e) {
+            alert('terjadi kesalahan');
         }
     }
 
     render(){
+        if(this.state.isLoading==true){
+            return(
+                <View>
+                    <Text>
+                        Loadingg...
+                    </Text>
+                </View>
+            )
+        }
+        
+        if(this.state.token==''){
+            // this.handleGoBack()
+            this.props.navigation.navigate('Login')
+        }
         return(
             <Container>
                 <Content>
                     <Text>
                         Home
                     </Text>
-                    <Button onPress={()=>this.props.navigation.navigate("Login")}>
-                        <Text> Login </Text>
+                    <Text>
+                        {this.state.token}
+                    </Text>
+                    <Button onPress={()=>this.handleLogoutButton()}>
+                        <Text> Logout </Text>
                     </Button>
                 </Content>
             </Container>
