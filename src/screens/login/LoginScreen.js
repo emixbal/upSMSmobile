fb55102447cf7d6a7bdaafdb89dd91b227e737c6
iqbal <emixbal@gmail.com>
2019-08-27 17:12:40 +0700

    create login and disable back button

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -1,6 +1,6 @@
 import React, {Component, Fragment} from 'react';
-import {StyleSheet} from 'react-native';
-import { Container, Content, Body, Title, Form, Input, Item, Header, Button, Text} from 'native-base';
+import {AsyncStorage, StyleSheet, BackHandler, Alert} from 'react-native';
+import { Container, Content, Form, Input, Item, Button, Text, View} from 'native-base';
 
 
 class LoginScreen extends React.Component {
@@ -11,33 +11,79 @@ class LoginScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: false,
+            isLoading: false,
             email: '',
-            password: ''
+            password: '',
         }
     }
 
-    handleButtonLogin(){
-        this.props.navigation.navigate('Home')
+    componentDidMount() {
+        this.backHandler = BackHandler.addEventListener("hardwareBackPress", () => {
+            return this.props.navigation.navigate("Login");
+        });
+    }
+    
+    componentWillUnmount() {
+        this.backHandler.remove();
+    }
+
+    async handleButtonLogin(){
+        try {
+            await this.setState({isLoading:true});
+            if (this.state.email=='Qaz' && this.state.password=='123'){
+                await this.setState({isLoading:false});
+                await AsyncStorage.setItem('token', '123');
+                await this.props.navigation.navigate('Home');
+            } else{
+                alert('email atau password salah')
+                await this.setState({isLoading:false});
+            }
+        } catch (e) {
+            alert('terjadi kesalahan');
+        }
     }
 
     render(){
+        if(this.state.isLoading==true){
+            return(
+                <View>
+                    <Text>
+                        Loading... Soon will be replace with modal spinner...
+                    </Text>
+                </View>
+            )
+        }
         return(
             <Container>
                 <Content>
                     <Form>
                         <Item>
-                            <Input placeholder="email" />
+                            <Input
+                                placeholder="email"
+                                value={this.state.email}
+                                onChangeText={text=>this.setState({email:text})}
+                            />
                         </Item>
                         <Item>
-                            <Input placeholder="password" />
+                            <Input
+                                secureTextEntry={true}
+                                placeholder="password" 
+                                value={this.state.password}
+                                onChangeText={text=>this.setState({password:text})}
+                            />
                         </Item>
-                        <Button info onPress={()=>this.handleButtonLogin()}><Text> Login </Text></Button>
+                        <Button style={styles.loginBtn} info onPress={()=>this.handleButtonLogin()}><Text> Login </Text></Button>
                     </Form>
                 </Content>
             </Container>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    loginBtn: {
+        marginTop: 20,
+    }
+});
 
 export default LoginScreen;
