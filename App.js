ec9cea6808fbe67cb98ae1a0e2aba4682d7211c8
iqbal <emixbal@gmail.com>
2019-08-27 13:59:47 +0700

    disable yellow box

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
 import React, {Component, Fragment} from 'react';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+    'Warning: componentWillMount is deprecated',
+    'Warning: componentWillUpdate is deprecated',
+    'Warning: componentWillReceiveProps is deprecated',
+]);
 
 // import LoginScreen from './src/screens/login/LoginScreen.js';
 
